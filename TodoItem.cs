2ce2ee18655556e6da65ced82812c6b1aa622e27
Harrison Cutler <harrisoncutler5@gmail.com>
2020-03-25 11:31:24 +0000

    Added TodoItem class

---
+++ TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoListAPI.Models
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+        public bool Required { get; set; }
+        public string Description { get; set; }
+    }
+}
